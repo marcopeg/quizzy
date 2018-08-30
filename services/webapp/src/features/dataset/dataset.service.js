7c73f5e21a8455a22f81c7b98b51ae6142f42705
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-08-30 15:02:43 +0200

    improves

--- services/webapp/src/features/dataset/dataset.service.js
+++ services/webapp/src/features/dataset/dataset.service.js
@@ -3,16 +3,63 @@ import { getJSON } from '@marcopeg/utils/lib/request'
 import {
     setDataset,
     setReady,
+    setLoading,
 } from './dataset.reducer'
 
-export const fetchDataset = () => (dispatch, getState) => {
+export const fetchRemoteDataset = () => (dispatch, getState) => {
     const { ssr } = getState()
     const endpoint = ssr.apiUrl('/v1/dataset/')
     return ssr.await(getJSON(endpoint))
 }
 
+export const readLocalDataset = () => () => {
+    try {
+        return JSON.parse(localStorage.getItem('quizzy:dataset'))
+    } catch (err) {
+        return null
+    }
+}
+
+export const writeLocalDataset = (dataset) => () => {
+    localStorage.setItem('quizzy:dataset', JSON.stringify(dataset))
+}
+
+export const download = () => async (dispatch) => {
+    let data = null
+    try {
+        dispatch(setLoading(true))
+        data = await dispatch(fetchRemoteDataset())
+    } catch (err) {
+        throw new Error(`[dataset] failed download - ${err.message}`)
+    } finally {
+        dispatch(setLoading(false))
+    }
+    return data
+}
+
+export const resync = () => async (dispatch) => {
+    try {
+        const dataset = await dispatch(download())
+        dispatch(writeLocalDataset(dataset))
+        dispatch(setDataset(dataset))
+        dispatch(setReady())
+    } catch (err) {
+        // eslint-disable-next-line
+        alert('it was not possible to sync the dataset')
+    }
+}
+
 export const start = () => async (dispatch) => {
-    const dataset = await dispatch(fetchDataset())
-    dispatch(setDataset(dataset))
-    dispatch(setReady())
+    try {
+        let dataset = dispatch(readLocalDataset())
+        if (!dataset) {
+            dataset = await dispatch(download())
+            dispatch(writeLocalDataset(dataset))
+        }
+        dispatch(setDataset(dataset))
+        dispatch(setReady())
+    } catch (err) {
+        // eslint-disable-next-line
+        alert('it was not possible to sync the dataset')
+    }
 }
