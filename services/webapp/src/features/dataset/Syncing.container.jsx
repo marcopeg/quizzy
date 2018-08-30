5af3e40c43603ce75fbd4e15168feb7050d4ae8d
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-08-30 17:20:14 +0200

    improves

--- services/webapp/src/features/dataset/Syncing.container.jsx
+++ services/webapp/src/features/dataset/Syncing.container.jsx
@@ -16,12 +16,12 @@ const style = {
         alignItems: 'center',
         transition: 'transform 0.3s ease',
     },
-    visible: {
-        transform: 'translate3d(0, 0, 0)',
-    },
-    hidden: {
-        transform: 'translate3d(0, 100vh, 0)',
-    },
+    // visible: {
+    //     transform: 'translate3d(0, 0, 0)',
+    // },
+    // hidden: {
+    //     transform: 'translate3d(0, 100vh, 0)',
+    // },
     message: {
         color: '#fff',
         fontSize: 40,
